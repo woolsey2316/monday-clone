63001bac5ff703cbdef874436917981b27698d25
David Woolsey <woolsey2316@gmail.com>
2025-10-11 16:58:07 +1100

    feat: convert to typescript

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,11 +2,13 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import json from "@eslint/json";
 
 export default [
   { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -22,12 +24,24 @@ export default [
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+    },
+  },
+  {
+    files: ["**/*.json", "**/*.jsonc", "**/*.json5"],
+    plugins: {
+      json,
+    },
+    language: "json/json",
+    rules: {
+      "json/no-duplicate-keys": "error",
+      // Add other JSON-specific rules here
     },
   },
 ]
